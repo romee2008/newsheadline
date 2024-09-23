d9b9e655ee76b195a5668c9e35a337021dfe1adc
romee2008 <romeeweb@gmail.com>
2024-09-23 21:05:49 +1000

    Update news_headlines.py

--- news_headlines.py
+++ news_headlines.py
@@ -8,20 +8,7 @@ from os import environ
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-"""
-consumer_key1 = 'o6bF5T5It9WtHjGTVbR1zmbB1'
-consumer_secret_key1 = 'uDEcRuOemqjQwaiB5pHy0aobtJNjiCq6ztHLDhpY1i54Pzv1AA'
-access_token1 = '871329457595744256-Tb9ijBkXGIaqsDNcXZorqxGv9ID8mkU'
-access_token_secret1 = 'HNg93RFiC2g5b50gux5aeZpTJltEv8c37T6K4CfKfmzWR'
-"""
-"""
-consumer_key1 = credentials.CONSUMER_KEY
-consumer_secret_key1 = credentials.CONSUMER_SECRET_KEY
-access_token1 = credentials.ACCESS_TOKEN
-access_token_secret1 = credentials.ACCESS_TOKEN_SECRET
 
-
-"""
 consumer_key1 = environ['CONSUMER_KEY']
 consumer_secret_key1 = environ['CONSUMER_SECRET_KEY']
 access_token1 = environ['ACCESS_TOKEN']
