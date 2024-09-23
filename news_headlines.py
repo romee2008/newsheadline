4ea8bd0ce88d3763ad4f2024a8a59b6e04d3bcdc
romee2008 <romeeweb@gmail.com>
2024-09-23 22:04:32 +1000

    Update news_headlines.py

--- news_headlines.py
+++ news_headlines.py
@@ -89,6 +89,3 @@ if __name__ == "__main__":
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
 }
-
-response = requests.get('https://www.forexlive.com', headers=headers)
-soup = BeautifulSoup(response.text, 'html.parser')
